126290d7eb3234832f90a144f56a9415e3d3186c
katelynn kimiko mitsunaga <kmitsunaga@linux10606.dc.engr.scu.edu>
2026-06-04 23:58:28 -0700

    forgot to add everything T.T

---
+++ loony_lists/term/list.c
@@ -0,0 +1,251 @@
+/* Kimi Mitsunaga
+ * Term Project: Loony Lists
+ * June 5th, 2026
+ *
+ * Project Description:
+ *
+ *   
+ *   
+ */
+
+#include <stdio.h>
+#include<assert.h>
+#include <stdlib.h>
+
+struct node{
+
+    struct node* prev;
+    struct node* next;
+
+    void** data;
+    int first;
+
+    int countA;         //count of elements in array
+    int length;
+
+};
+
+struct list{
+
+    struct node* header;
+    int countN;             //count of nodes in list
+    int (*compare)();
+
+};
+
+
+// ------------ FUNCTIONS ------------- //
+
+//TAKEN FROM LAB 5.2 - mknode();
+
+//Purpose: creates new node
+//Parameters: left and right nodes, count,
+//Returns: new node pointer
+//Runtime: O(1)
+
+struct node* mknode( int count, struct node* prev, struct node* next){ // p = prev, n = next
+
+    assert( count >= 0 );
+
+    struct node* new = malloc( sizeof( struct node ) );
+
+
+    return new;
+
+}
+
+/* Purpose: creates new node
+// Parameters: function pointer compare()
+// Returns: new list pointer
+// Runtime: O(1)
+*/
+
+struct LIST *createList( int (*compare)() ){
+
+    struct LIST *lp = malloc( sizeof( struct LIST) );
+
+    node* dummy = malloc( sizeof( struct node ) );
+
+    lp->header = dummy;
+    lp->header->next = dummy;
+    lp->header->prev = dummy;
+
+    lp->header->length = 0;
+    lp->header->countA = 0;
+
+    lp->compare = compare;
+
+    return lp;
+
+}
+
+void destroyList(LIST *lp){
+    
+    struct node* temp;
+
+    while(lp->header->next != lp->header){
+        temp = lp->heder->next;
+        lp->header->next = temp->next;
+        free(temp->data);
+        free(temp);
+    }
+
+    free(lp->header);
+    free(temp);
+
+}
+
+int numItems(LIST *lp){
+
+    assert( lp != NULL);
+    return lp->countN;
+
+}
+
+void addFirst(LIST *lp, void *item){
+
+    assert( lp != NULL && item != NULL);
+    struct node curr;
+
+    if(lp->countN == 0){
+        curr = mknode(lp, lp->header, lp->header);
+    }
+    else{
+        curr = lp->header->next;
+    }
+
+    if( curr->countA == curr->length){
+        curr = mknode(lp, lp->header, curr);
+    }
+    if( curr->countA != 0){
+        curr->first = (curr->first - 1 + curr->length ) % curr->length;
+    }
+
+    curr->data[curr->first] = item;
+    curr->countA++;
+    lp->countN++;
+
+
+}
+void addLast(LIST *lp, void *item){
+
+    assert (lp != NULL && item != NULL);
+
+    struct node *curr = lp->header->prev;
+    int last;
+
+    if (lp->countN == 0){
+
+        curr = mknode(lp, lp->header, lp->header);
+    }
+    else{
+        curr = lp->header->prev;
+    }
+    if (curr->countA == curr->length){
+        curr = mknode(lp, curr, lp->header);
+    }
+
+    last = (curr->first + curr->countA) % curr->length;
+
+    curr->data[last] = item;
+    curr->countA++;
+    lp->countN++;
+}
+
+void *removeFirst(LIST *lp){
+
+    assert (lp != NULL);
+
+    if (lp->countN == 0){
+        return NULL;
+    }
+
+    struct node *temp = lp->header->next;
+    struct node *curr = lp->header->next;
+
+    void *deletedVal;
+
+    if (curr->countA == 0) {
+
+        temp = curr;
+        curr = curr->next;
+        curr->prev = lp->header;
+        lp->header->next = curr;
+
+        free(temp->data);
+        free(temp);
+
+    }
+
+    value = curr->data[curr->first];
+    curr->data[curr->first] = NULL;
+    curr->first = (curr->first + 1) % curr->length;
+    curr->countA--;
+    lp->countN--;
+
+    return value;
+
+}
+
+void *removeLast(LIST *lp){
+
+    assert (lp != NULL);
+
+    if (lp->countN == 0){
+        return NULL;
+    }
+
+    struct node *temp = lp->header->prev;
+    struct node *curr = lp->header->prev;
+
+    void *value;
+    int last;
+
+    if (curr->countA == 0) {
+
+        temp = curr;
+        curr = curr->prev;
+        curr->next = lp->header;
+        lp->header->prev = curr;
+
+        free(temp->data);
+        free(temp);
+    }
+
+    last = (curr->first + curr->countA - 1) % curr->length;
+
+    value = curr->data[last];
+    curr->data[last] = NULL;
+    curr->countA--;
+    lp->countN--;
+
+    return value;
+
+}
+
+void *getItem(LIST *lp, int index){
+
+    if (lp->countN == 0) {
+
+        printf("List is empty, no elements\n");
+        return NULL;
+    }
+
+    else if (index >= lp->countN || index < 0) {
+
+        printf("Index is invalid\n");
+        return NULL;
+    }
+
+    else {
+
+        struct node *curr = lp->header->next;
+
+        while (index - curr->length >= 0 && curr->length != 0) {
+
+            index -= curr->length;
+            curr = curr->next;
+        }
+        return curr->data[(curr->first + index) % curr->length];
+    }
+}
